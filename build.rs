5c9a72a0b64e696687c75f246d2b6f593f8f04e2
Joel Gallant <joel@joelgallant.me>
2018-09-24 08:36:27 -0600

    Uses compile_error macro instead of panicking in build.rs

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    if ! cfg!(target_os = "linux") || cfg!(target_os = "freebsd") {
-        println!("rscam (v4l2) is for linux/freebsd only");
-        std::process::exit(1);
-    }
-}
